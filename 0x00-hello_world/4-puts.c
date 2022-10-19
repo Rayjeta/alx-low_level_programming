e06f3939190a9718e0118b315a758e51d038190f
kg-stien <marvunumadu@gmail.com>
2022-10-19 09:37:47 +0100

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - prints text
+ *
+ * Return:0
+ **/
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return(0);
+
+}
+
